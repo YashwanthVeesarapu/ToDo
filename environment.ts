b16f616269474fedbe8ff76ab1c45ce190b93365
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2025-03-15 19:13:11 -0400

    google analystics

--- environment.ts
+++ environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  // apiUrl: 'https://redtodo-91a009c1eda0.herokuapp.com/',
-  apiUrl: 'https://s.todo.redsols.com',
-  // apiUrl: 'http://localhost:7001',
+  // apiUrl: 'https://s.todo.redsols.com',
+  apiUrl: 'http://localhost:7001',
 };
