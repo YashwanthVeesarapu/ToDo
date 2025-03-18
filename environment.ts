74a4a6c80c8bcd8c562391b2f32aee6573e7221b
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2025-03-18 00:51:32 -0400

    todo uid bug fix

--- environment.ts
+++ environment.ts
@@ -1,4 +1,9 @@
+const url =
+  window.location.hostname == 'localhost'
+    ? 'http://localhost:7001'
+    : 'https://s.todo.redsols.com';
+
 export const environment = {
   // apiUrl: 'https://s.todo.redsols.com',
-  apiUrl: 'http://localhost:7001',
+  apiUrl: url,
 };
